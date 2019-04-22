bb1d00668e15b38b97f25831bf832537bd38bf89
morfiy <edurnev@keaz.ru>
2019-04-22 15:24:55 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -22,3 +22,11 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+$(document).ready(function(){
+    $(".footer-links").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
